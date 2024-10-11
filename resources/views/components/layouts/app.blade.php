c8a0d76876affa63b27ce1286485419e5d3b57a0
Tim Kleyersburg <tk@wacg.de>
2024-10-11 08:58:19 +0200

    add tracking script

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -13,6 +13,9 @@
         content="A gradient palette generator for Minecraft. Quickly find matching blocks between two other blocks."
     />
     @vite('resources/css/app.css')
+    @production
+        <script defer src="https://c3po.wacg.dev/protocol.js" data-website-id="31fca00c-9f17-4570-bd70-bd58b85b22af"></script>
+    @endproduction
 </head>
 <body class="m-6 bg-stone-300 text-stone-700 lg:m-12">
     {{ $slot }}
